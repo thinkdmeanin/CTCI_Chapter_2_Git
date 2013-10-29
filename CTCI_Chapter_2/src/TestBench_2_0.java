cda550569d1c74a834c15157402558890a957ead
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-29 13:26:40 -0400

    2-0 Basic Knowledge

---
+++ CTCI_Chapter_2/src/TestBench_2_0.java
@@ -0,0 +1,15 @@
+public class TestBench_2_0 {
+	Node deleteNode (Node head, int d) {
+		Node n = head;
+		if (n.data == d) {
+			return head.next; /*moved head */
+		}
+		while (n.next != null) {
+			if(n.next.data == d) {
+				n.next = n.next.next;
+				return head; /*head didn't change*/
+			}
+			n = n.next;
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+Cracking the Code Interview My trial code
+
+Zheming Xin
+10/2013
